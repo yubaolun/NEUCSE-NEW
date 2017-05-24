47525cb5206b56e9b8270bee0d3f6cd6bfb1a84c
yubaolun <yubaolun@qq.com>
2017-05-24 10:01:40 +0800

    first commit

---
+++ treeView/views.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render_to_response, render
+from treeView.models import treeView
+
+
+# Create your views here.
+
+
+
+def tree(request):
+    nodes = treeView.objects.all()
+    return render_to_response("treeView.html", {'nodes': nodes})
+
+
+def index(request):
+    nodes = treeView.objects.all()
+    return render_to_response("index2.html", {'nodes': nodes})
+
+
+def institude(request):
+    nodes = treeView.objects.all()
+    return render_to_response("组织机构.html", {'nodes': nodes})
+
+
+def uniform_page(request, page_name):
+    nodes = treeView.objects.all()
+    page = page_name + '.html'
+    return render_to_response(page, {'nodes': nodes})
